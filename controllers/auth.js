a20af5ec077c6f45ec0217866ca74b3ba80a5451
Breison Mendez <Mendez12361@outlook.com>
2023-04-12 17:46:00 -0500

    Google sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const Usuario = require('../models/Usuario');
 const bcrypt = require('bcryptjs'); 
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 const login = async( req, res = response ) => {
 
     const { email , password } = req.body;
@@ -43,6 +44,50 @@ const login = async( req, res = response ) => {
     }
 }
 
+const googleSignIn = async( req, res = response ) => {
+
+    try {
+        const { email , name , picture } = await googleVerify( req.body.token );
+        //Comprobar que no exista el email
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if( !usuario ){
+            usuario = new Usuario({
+                nombre: name ,
+                email,
+                password: "@@@",
+                img: picture,
+                google: true
+            }); 
+        }else{
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+        //Guardar usuario
+        await usuario.save();
+
+        //Generar token
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            ok: true,
+            email , name , picture ,
+            token
+        });
+
+    } catch (error) {
+        console.log( error );
+        res.status(400).json({
+            ok: true,
+            msg: 'Token de Google no es correcto'
+        })    
+    }
+          
+    
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
